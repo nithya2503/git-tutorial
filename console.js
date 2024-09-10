3be72860bbeed291b6f4b4d4b7d04e48e293ea4c
nithyasri <kattelanithyasri9100@gmail.com>
2024-09-10 17:26:03 -0400

    Version 3

--- console.js
+++ console.js
@@ -1,2 +1,1 @@
-console.log('version 2');
-console.log('version2 updated');
+console.log('my version 3');
--- wine_prediction.py
+++ wine_prediction.py
@@ -1,6 +1,6 @@
 # Python Program to find the area of triangle
 
-a = 6
+a = 3
 b = 6
 c = 7
 
